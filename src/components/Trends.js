7e18f8d8fe1edfda42c18bdaf1719fb90ee0a168
Rowa <jrowah@gmail.com>
2023-04-04 13:17:27 +0300

    adds like/retweet/share tweet buttons

--- src/components/Trends.js
+++ src/components/Trends.js
@@ -158,7 +158,7 @@ function WhoToFollow() {
 
 function TrendTopics() {
   return (
-    <div class="ml-6 mt-6 p-3 bg-gray-200 rounded-lg">
+    <div class="ml-6 mt-6 p-3 bg-gray-200 rounded-lg sticky top-0 z-50">
       <h1 class="font-bold text-lg">Trends for you</h1>
       <div class="mt-3">
         <h1 class="text-gray-500">Sports . Trending</h1>
@@ -169,6 +169,26 @@ function TrendTopics() {
         <h1 class="text-gray-500">Trending in Kenya</h1>
         <h2 class="font-bold">Vikings</h2>
         <h3 class="text-gray-500">10k Tweets</h3>
+      </div>
+      <div class="mt-3">
+        <h1 class="text-gray-500">Trending in Kenya</h1>
+        <h2 class="font-bold">Erick Omondi</h2>
+        <h3 class="text-gray-500">15k Tweets</h3>
+      </div>
+      <div class="mt-3">
+        <h1 class="text-gray-500">Technology . Trending</h1>
+        <h2 class="font-bold">#OpenAI</h2>
+        <h3 class="text-gray-500">15k Tweets</h3>
+      </div>
+      <div class="mt-3">
+        <h1 class="text-gray-500">Trending in Kenya</h1>
+        <h2 class="font-bold">Nanyuki</h2>
+        <h3 class="text-gray-500">1,000 Tweets</h3>
+      </div>
+      <div class="mt-3">
+        <h1 class="text-gray-500">Trending</h1>
+        <h2 class="font-bold">#HBOMax</h2>
+        <h3 class="text-gray-500">1,101 Tweets</h3>
       </div>
       <div class="mt-3">
         <h1 class="text-gray-500">Trending</h1>
