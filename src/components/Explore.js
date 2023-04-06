33ca6acfce82ac10ca367732eca522a6abb8fa9e
Rowa <jrowah@gmail.com>
2023-04-06 14:05:55 +0300

    adds new pages with existing components

---
+++ src/components/Explore.js
@@ -0,0 +1,15 @@
+import Aside from "./Aside";
+import WhoToFollow from "./WhoToFollow";
+
+function Main() {
+  return <div class="w-3/5 border border-gray-200 h-auto  border-t-0"></div>;
+}
+export default function Explore() {
+  return (
+    <div class="mx-20 mt-4 flex">
+      <Aside />
+      <Main />
+      <WhoToFollow />
+    </div>
+  );
+}
