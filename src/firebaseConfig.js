4526bdef5976c00ef596ef4c125f9fa17d102a60
Rowa <jrowah@gmail.com>
2023-04-14 19:33:54 +0300

    adds firebase and tailwind

---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCus-ojRcHEd7NV6H9aL_i45_YjrjQSoT0",
+  authDomain: "twitter-clone-2d12c.firebaseapp.com",
+  projectId: "twitter-clone-2d12c",
+  storageBucket: "twitter-clone-2d12c.appspot.com",
+  messagingSenderId: "664052892661",
+  appId: "1:664052892661:web:8f017a9acb93799cf596bc",
+  measurementId: "G-003EFX783X",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
