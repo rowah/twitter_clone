99a1add5e0202d921571d5d05fbd76412b1a297a
Rowa <jrowah@gmail.com>
2023-04-17 14:40:23 +0300

    adds antd lib loading page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HomeComponent from "../components/HomeComponent";
 import { onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebaseConfig";
+import Loader from "../components/common/Loader";
 
 export default function Home() {
+  const [loading, setLoading] = useState(true);
   let navigate = useNavigate();
   useEffect(() => {
     onAuthStateChanged(auth, (res) => {
       console.log(res?.accessToken);
       if (!res?.accessToken) {
         navigate("/");
+      } else {
+        setLoading(false);
       }
     });
   }, []);
-  return (
-    <div>
-      <HomeComponent />
-    </div>
-  );
+  return loading ? <Loader /> : <HomeComponent />;
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import LoginComponent from "../components/LoginComponent";
 import { auth } from "../firebaseConfig";
+import Loader from "../components/common/Loader";
 export default function Login() {
+  const [loading, setLoading] = useState(true);
   let navigate = useNavigate();
   useEffect(() => {
     onAuthStateChanged(auth, (res) => {
@@ -12,8 +13,10 @@ export default function Login() {
       //if there is access token, navigate to home page
       if (res?.accessToken) {
         navigate("/home");
+      } else {
+        setLoading(false);
       }
     });
   }, []);
-  return <LoginComponent />;
+  return loading ? <Loader /> : <LoginComponent />;
 }
