421ab904644900c5101b7464ad9f2ea348a16c20
Rowa <jrowah@gmail.com>
2023-04-17 12:18:42 +0300

    manages auth state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomeComponent from "../components/HomeComponent";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebaseConfig";
 
 export default function Home() {
-  return <HomeComponent />;
+  let navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, (res) => {
+      console.log(res?.accessToken);
+      if (!res?.accessToken) {
+        navigate("/");
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <HomeComponent />
+    </div>
+  );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import LoginComponent from "../components/LoginComponent";
+import { auth } from "../firebaseConfig";
 export default function Login() {
+  let navigate = useNavigate();
+  useEffect(() => {
+    onAuthStateChanged(auth, (res) => {
+      console.log(res?.accessToken);
+      //if there is access token, navigate to home page
+      if (res?.accessToken) {
+        navigate("/home");
+      }
+    });
+  }, []);
   return <LoginComponent />;
 }
