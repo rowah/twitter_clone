25e413725225304a3fa941f65f84778eabd53dc9
Rowa <jrowah@gmail.com>
2023-04-18 20:28:34 +0300

    adds home page

--- src/components/common/sidebar/Aside.jsx
+++ src/components/common/sidebar/Aside.jsx
@@ -1,3 +1,6 @@
+import { RiHome7Fill } from "react-icons/ri";
+import { HiOutlineBell } from "react-icons/hi";
+import { BsThreeDots } from "react-icons/bs";
 function Aside() {
   return (
     <aside className="flex flex-col w-1/4 h-screen px-2 py-6 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700 sticky top-0 z-50">
@@ -17,27 +20,15 @@ function Aside() {
             href="/home"
             className="group flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full hover:bg-gray-300 focus:bg-gray-300  text-black"
           >
-            <svg
-              className="mr-4 h-6 w-6 "
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 12l9-9 9 9M5 10v10a1 1 0 001 1h3a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1h3a1 1 0 001-1V10M9 21h6"
-              />
-            </svg>
-            Home
+            <RiHome7Fill size={25} />
+            <span className="ml-3 text-xl">Home</span>
           </a>
           <a
             href="/explore"
             className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-semibold rounded-full hover:bg-gray-300 hover:text-black"
           >
             <svg
-              className="mr-4 h-6 w-6"
+              className="h-6 w-6"
               fill="none"
               strokeLinecap="round"
               strokeLinejoin="round"
@@ -47,24 +38,14 @@ function Aside() {
             >
               <path d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"></path>
             </svg>
-            Explore
+            <span className=" ml-3 text-xl">Explore</span>
           </a>
           <a
             href="/notifications"
             className="mt-1 group flex items-center px-2 py-2 text-base leading-6 font-medium rounded-full hover:bg-gray-300 hover:text-black"
           >
-            <svg
-              className="mr-4 h-6 w-6"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
-            </svg>
-            Notifications
+            <HiOutlineBell size={25} />
+            <span className="ml-3 text-xl">Notifications</span>
           </a>
           <a
             href="/messages"
@@ -81,7 +62,7 @@ function Aside() {
             >
               <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path>
             </svg>
-            Messages
+            <span className="text-xl">Messages</span>
           </a>
           <a
             href="bookmarks/"
@@ -98,7 +79,7 @@ function Aside() {
             >
               <path d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path>
             </svg>
-            Bookmarks
+            <span className="text-xl">Bookmarks</span>
           </a>
           <a
             href="/profile"
@@ -115,7 +96,7 @@ function Aside() {
             >
               <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
             </svg>
-            Twitter Blue
+            <span className="text-xl">Twitter Blue</span>
           </a>
           <a
             href="/profile"
@@ -132,7 +113,7 @@ function Aside() {
             >
               <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
             </svg>
-            Profile
+            <span className="text-xl">Profile</span>
           </a>
           <a
             href="/home"
@@ -149,7 +130,7 @@ function Aside() {
             >
               <path d="M8 12h.01M12 12h.01M16 12h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
             </svg>
-            More
+            <span className="text-xl">More</span>
           </a>
 
           <button className="bg-blue-400 w-48 mt-5 hover:bg-blue-600 text-black font-bold py-2 px-4 rounded-full">
@@ -164,16 +145,19 @@ function Aside() {
                 <img
                   className="inline-block h-10 w-10 rounded-full"
                   src="https://pbs.twimg.com/profile_images/1504385195037843457/p5kNTiBq_400x400.jpg"
-                  alt="ppic"
+                  alt="user profile image"
                 />
               </div>
               <div className="ml-3">
                 <p className="text-base leading-6 font-medium text-black">
-                  Jay Rowah
+                  User Name
                 </p>
                 <p className="text-sm leading-5 font-medium text-gray-400 group-hover:text-gray-300 transition ease-in-out duration-150">
-                  @JRowah
+                  @username
                 </p>
+              </div>
+              <div className="ml-20">
+                <BsThreeDots />
               </div>
             </div>
           </a>
---
+++ src/components/common/trends/TrendTopics.jsx
@@ -0,0 +1,204 @@
+import Policy from "../policies/Policy";
+export default function TrendTopics() {
+  return (
+    <div class="sticky top-20 z-50">
+      <div class="ml-6 mt-6 p-3 bg-gray-200 rounded-lg ">
+        {/* <div class="max-w-sm rounded-lg bg-gray-200 overflow-hidden shadow-lg m-4 mr-20"> */}
+        <div class="flex">
+          <div class="flex-1 m-2">
+            <h2 class="px-4 py-2 text-xl w-48 font-semibold text-black">
+              Kenya trends
+            </h2>
+          </div>
+          <div class="flex-1 px-4 py-2 m-2">
+            <a
+              href="/"
+              class=" text-2xl rounded-full text-black hover:bg-blue-800 hover:text-blue-300 float-right"
+            >
+              <svg
+                class="m-2 h-6 w-6"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path>
+                <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        {/* <!--first trending tweet--> */}
+        <div class="flex">
+          <div class="flex-1">
+            <p class="px-4 ml-2 mt-3 w-48 text-xs text-gray-600">
+              1 . Trending
+            </p>
+            <h2 class="px-4 ml-2 w-48 font-bold text-black">#Kilimani</h2>
+            <p class="px-4 ml-2 mb-3 w-48 text-xs text-gray-600">
+              5,466 Tweets
+            </p>
+          </div>
+          <div class="flex-1 px-4 py-2 m-2">
+            <a
+              href="/"
+              class=" text-2xl rounded-full text-gray-400 hover:bg-blue-800 hover:text-blue-300 float-right"
+            >
+              <svg
+                class="m-2 h-5 w-5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 9l-7 7-7-7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        {/* <!--second trending tweet--> */}
+
+        <div class="flex">
+          <div class="flex-1">
+            <p class="px-4 ml-2 mt-3 w-48 text-xs text-gray-600">
+              2 . Politics . Trending
+            </p>
+            <h2 class="px-4 ml-2 w-48 font-bold text-black">#DPGachagua</h2>
+            <p class="px-4 ml-2 mb-3 w-48 text-xs text-gray-600">
+              8,464 Tweets
+            </p>
+          </div>
+          <div class="flex-1 px-4 py-2 m-2">
+            <a
+              href="/"
+              class=" text-2xl rounded-full text-gray-400 hover:bg-blue-800 hover:text-blue-300 float-right"
+            >
+              <svg
+                class="m-2 h-5 w-5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 9l-7 7-7-7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        <div class="flex">
+          <div class="flex-1">
+            <p class="px-4 ml-2 mt-3 w-48 text-xs text-gray-600">
+              3 . Trending
+            </p>
+            <h2 class="px-4 ml-2 w-48 font-bold text-black">#Shaffie</h2>
+            <p class="px-4 ml-2 mb-3 w-48 text-xs text-gray-600">
+              5,466 Tweets
+            </p>
+          </div>
+          <div class="flex-1 px-4 py-2 m-2">
+            <a
+              href="/"
+              class=" text-2xl rounded-full text-gray-400 hover:bg-blue-800 hover:text-blue-300 float-right"
+            >
+              <svg
+                class="m-2 h-5 w-5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 9l-7 7-7-7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        {/* <!--third trending tweet--> */}
+
+        <div class="flex">
+          <div class="flex-1">
+            <p class="px-4 ml-2 mt-3 w-48 text-xs text-gray-600">
+              4 . Rock . Trending
+            </p>
+            <h2 class="px-4 ml-2 w-48 font-bold text-black">#Ferrari</h2>
+            <p class="px-4 ml-2 mb-3 w-48 text-xs text-gray-600">
+              5,586 Tweets
+            </p>
+          </div>
+          <div class="flex-1 px-4 py-2 m-2">
+            <a
+              href="/"
+              class=" text-2xl rounded-full text-gray-400 hover:bg-blue-800 hover:text-blue-300 float-right"
+            >
+              <svg
+                class="m-2 h-5 w-5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 9l-7 7-7-7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        {/* <!--forth trending tweet--> */}
+
+        <div class="flex">
+          <div class="flex-1">
+            <p class="px-4 ml-2 mt-3 w-48 text-xs text-gray-600">
+              5 . Auto Racing . Trending
+            </p>
+            <h2 class="px-4 ml-2 w-48 font-bold text-black">#vettel</h2>
+            <p class="px-4 ml-2 mb-3 w-48 text-xs text-gray-600">
+              9,416 Tweets
+            </p>
+          </div>
+          <div class="flex-1 px-4 py-2 m-2">
+            <a
+              href="/"
+              class=" text-2xl rounded-full text-gray-400 hover:bg-blue-800 hover:text-blue-300 float-right"
+            >
+              <svg
+                class="m-2 h-5 w-5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M19 9l-7 7-7-7"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+
+        {/* <!--show more--> */}
+
+        <div class="flex">
+          <div class="flex-1 p-4">
+            <a href="kh.c">
+              <h2 class="px-4 ml-2 w-48 font-bold text-blue-400">Show more</h2>
+            </a>
+          </div>
+        </div>
+      </div>
+      <Policy />
+    </div>
+  );
+}
